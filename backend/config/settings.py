f3e879fe5def9682053bb72d7de29ff5bed710a4
Joeverly Lanzaderas <laisasambaan05@gmail.com>
2026-05-24 00:24:45 +0800

    Update settings.py7

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-dev-secret-key-change-in-production')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'librium.onrender.com']
 
