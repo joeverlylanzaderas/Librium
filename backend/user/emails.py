eb8c914833416b0e9532fb28c839ab9cd9d070ce
Hannah Manlapaz <zamoraarmada@gmail.com>
2026-05-24 22:46:52 +0800

    email activation2

--- backend/user/emails.py
+++ backend/user/emails.py
@@ -2,7 +2,7 @@ from djoser.email import ActivationEmail
 
 class CustomActivationEmail(ActivationEmail):
     # Points to the activation HTML file inside your templates
-    template_name = 'users/activation.html' 
+    template_name = 'activation.html' 
 
     def send(self, to, *args, **kwargs):
         # Force a subject line so Brevo/Anymail doesn't reject it
